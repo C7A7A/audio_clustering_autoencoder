fa9a1c8a77a40f99841bed11a79d227a02cba287
Czaja <czajkamateusz999@gmail.com>
2024-05-18 19:16:15 +0200

    save model checkpoints, pick best model (instead of last), user defined variables (dataset name, feature name)

---
+++ notebooks/FeaturesDataset.py
@@ -0,0 +1,30 @@
+from torch.utils.data import Dataset
+import numpy as np
+from PIL import Image
+
+WIDTH = 64
+HEIGHT = WIDTH // 2
+
+def load_img(path):
+    image = Image.open(path)
+    image = image.resize((WIDTH, HEIGHT))
+
+    if image.mode == 'RGBA':
+        image = image.convert('RGB')
+
+    return np.array(image)
+
+class FeaturesDataset(Dataset):
+    def __init__(self, paths, transform=None):
+        self.paths = paths
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.paths)
+    
+    def __getitem__(self, idx):
+        path = self.paths[idx]
+        feature = load_img(path)
+        if self.transform:
+            feature = self.transform(feature)
+        return feature
